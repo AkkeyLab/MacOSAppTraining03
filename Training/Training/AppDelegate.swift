e58a8b2aff761e2a8d1448e748082d74fd8ab963
AKIO <maizima44ww@gmail.com>
2016-08-31 22:36:35 +0900

    Add project files.

---
+++ Training/Training/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Training
+//
+//  Created by 板谷晃良 on 2016/08/31.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Training/Training/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by 板谷晃良 on 2016/08/31.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
