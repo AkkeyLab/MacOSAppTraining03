643cc9280c55c214fa03cbdad071871171e79278
AKIO <maizima44ww@gmail.com>
2016-09-03 02:46:21 +0900

    Full include.

--- Training/Training/ViewController.swift
+++ Training/Training/ViewController.swift
@@ -9,22 +9,53 @@
 import Cocoa
 
 class ViewController: NSViewController {
-    let task = NSTask()
+    private let task = NSTask()
+    private let alert = NSAlert()
+    private var homeDir: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        homeDir = NSHomeDirectory()
     }
 
     @IBAction func mkDir(sender: AnyObject) {
-        // let outputDir: String = NSHomeDirectory() + "/Desktop/AkkeyLab"
-        let outputDir: String = NSHomeDirectory() + "/AkkeyLab"
+//        let outputDir: String = homeDir + "/Desktop/AkkeyLab"
+        let outputDir: String = homeDir + "/AkkeyLab"
         NSLog("\(outputDir)")
 
         task.launchPath = "/bin/mkdir"
         task.arguments = [outputDir]
 
         task.launch()
+    }
+
+    @IBAction func cp(sender: AnyObject) {
+        let panel = NSOpenPanel()
+        // ファイル選択を許可させる
+        panel.canChooseFiles = true
+        panel.canChooseDirectories = false
+        panel.allowsMultipleSelection = false
+        panel.allowedFileTypes = NSImage.imageTypes()
+        panel.beginWithCompletionHandler({ (num) -> Void in
+            if num == NSModalResponseOK {
+                self.readItem((panel.URL?.path)!)
+            }
+        })
+    }
+
+    func readItem(url: String) {
+        if (NSImage(contentsOfFile: url) != nil) {
+            task.launchPath = "/bin/cp"
+            task.arguments = [url, homeDir + "/AkkeyLab/sample.png"]
+
+            task.launch()
+        } else {
+            alert.messageText = "Error"
+            alert.informativeText = "Not find file"
+            alert.runModal()
+        }
     }
 
     override var representedObject: AnyObject? {
